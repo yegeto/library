3b3f18844bb9f684879ea44ec0132ba4e570fc27
yegeto <115154804+yegeto@users.noreply.github.com>
2025-05-30 22:22:09 +0300

    Implement book card actions and finalize styles

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const library = document.querySelector(".library");
 const addBookBtn = document.querySelector(".add-book-btn");
 const addBookDialog = document.querySelector(".new-book-dialog");
 const addBookForm = document.querySelector(".new-book-form");
+const closeBtn = document.querySelector("#close-button");
 
 const myLibrary = [
   {
@@ -10,6 +11,13 @@ const myLibrary = [
     pages: 320,
     isRead: false,
     id: "1c64d75e-c07a-44eb-99e8-8c3c20685a0f",
+  },
+  {
+    title: "Crime and Punishment",
+    author: "Fyodor Dostoevsky",
+    pages: 720,
+    isRead: true,
+    id: "ae1f677c-9eef-4bdc-8cab-1e529a9b0299",
   },
 ];
 
@@ -29,8 +37,6 @@ function addBookToLibrary(title, author, pages, isRead) {
   myLibrary.push(newBook);
 }
 
-addBookToLibrary("Crime and Punishment", "Fyodor Dostoevsky", 720, true);
-
 function displayLibrary(myLibrary) {
   library.innerHTML = "";
   myLibrary.map((book) => {
@@ -39,18 +45,44 @@ function displayLibrary(myLibrary) {
     const bookAuthor = document.createElement("p");
     const bookPages = document.createElement("p");
     const bookRead = document.createElement("p");
+    const changeReadStatusBtn = document.createElement("button");
+    const deleteBookBtn = document.createElement("button");
+    const bookBtnWrapper = document.createElement("div");
 
     bookWrapper.setAttribute("id", `${book.id}`);
+    deleteBookBtn.setAttribute("class", "book-delete-btn");
+    changeReadStatusBtn.setAttribute("class", "change-read-status-btn");
+    bookBtnWrapper.setAttribute("class", "book-btn-wrapper");
 
     bookTitle.textContent = `${book.title}`;
     bookAuthor.textContent = `${book.author}`;
     bookPages.textContent = `${book.pages} Pages`;
     bookRead.textContent = `${book.isRead ? "Read" : "Not Read"}`;
+    changeReadStatusBtn.textContent = `${
+      book.isRead ? "Mark Not Read" : "Mark Read"
+    }`;
+    deleteBookBtn.textContent = "Delete";
+
+    const currentBookId = book.id;
+    changeReadStatusBtn.addEventListener("click", () => {
+      const book = myLibrary.find((book) => book.id === currentBookId);
+      book.isRead = !book.isRead;
+      displayLibrary(myLibrary);
+    });
+    deleteBookBtn.addEventListener("click", () => {
+      const updatedLibrary = myLibrary.filter(
+        (book) => book.id !== currentBookId
+      );
+      displayLibrary(updatedLibrary);
+    });
 
     bookWrapper.appendChild(bookTitle);
     bookWrapper.appendChild(bookAuthor);
     bookWrapper.appendChild(bookPages);
     bookWrapper.appendChild(bookRead);
+    bookBtnWrapper.appendChild(deleteBookBtn);
+    bookBtnWrapper.appendChild(changeReadStatusBtn);
+    bookWrapper.appendChild(bookBtnWrapper);
 
     library.appendChild(bookWrapper);
   });
@@ -70,6 +102,10 @@ addBookForm.addEventListener("submit", () => {
   addBookToLibrary(title, author, pages, isRead);
   displayLibrary(myLibrary);
   addBookForm.reset();
+});
+
+closeBtn.addEventListener("click", () => {
+  addBookDialog.close();
 });
 
 displayLibrary(myLibrary);
